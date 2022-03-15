47f638444ee5bc8ff3b703fafdcd0d9440d80554
damianyoung <74835887+damianyoung@users.noreply.github.com>
2022-03-14 21:45:03 -0400

    Readfile function
    Readfile functions created to populate number of customers and available resources

--- 190346060_190170320_a04_q1.c
+++ 190346060_190170320_a04_q1.c
@@ -6,38 +6,115 @@
 #include <sys/stat.h>
 #include <pthread.h>
 #include <time.h>
+#include <stdbool.h>
+#include <ctype.h>
 
-typedef struct Customer
+int *available_ptr;
+int *max_ptr;
+int *allocated_ptr;
+int *need_ptr;
+
+int customers = 0; // customers
+int resources = 0; // rows
+
+int readFileCustomer(char *fileName);
+void readFileSeq(char *fileName, int max[customers][resources]);
+bool safety(int *available, int *max, int *allocated, int *need);
+int sumArr(int arr[], int n);
+void findNumCol(char *fileName);
+void banker();
+void run();
+int request(int args[]);
+int release(int args[]);
+void status(int *available, int *max, int *allocated, int *need);
+
+typedef struct thread
 {
-    int customer_id;
-    int item1;
-    int item2;
-    int item3;
-    int item4;
-} Customer;
-
-int readFile(char *fileName, Customer *customer);
-int safetyAlgorithm(int customerCount);
-void requestResource(int threadID, int item1, int item2, int item3, int item4);
-void releaseResource(int threadID, int item1, int item2, int item3, int item4);
-void printValues(int customerCount);
-void runProgram(int customerCount);
-void *runThread(void *threadID);
-
-int available[5] = {0, 0, 0, 0, 0};
-
-Customer *customerMax = NULL;
-Customer *customerAlloc = NULL;
-Customer *customerNeed = NULL;
+    char tid[4];
+    unsigned int startTime;
+    int state;
+    pthread_t handle;
+    int ret;
+} Thread;
 
 int main(int argc, char *argv[])
 {
-    char *filename = "sample4_in.txt";
-
-    if (argc != 5)
+    findNumCol("sample4_in.txt");
+    customers = readFileCustomer("sample4_in.txt");
+    if (argc != resources + 1)
     {
-        printf("4 values must be entered to continue");
+        printf("Incorrect command line arguments\n");
+        return -1;
     }
 
+    printf("Total # of Customers: %d\n", customers);
+    printf("Current number of available resources: \n");
+    // Make two for loops below into one loop
+    for (int i = 1; i < resources; i++)
+    {
+        printf("%s ", argv[i]);
+    }
+    printf("%s\n", argv[resources]);
+
+    int *available = (int *)malloc(resources * sizeof(int));
+    for (int i = 0; i < resources; i++)
+    {
+        available[i] = atoi(argv[i + 1]);
+    }
+    // Make two for loops above into one loop
     return 0;
+}
+
+void findNumCol(char *fileName)
+{
+    FILE *fp = fopen(fileName, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error opening file!\n");
+        exit(1);
+    }
+
+    char *letter;
+    if (!feof(fp))
+    {
+        char line[100];
+        fgets(line, 100, fp);
+        letter = strtok(line, ",");
+        resources = 0;
+        while (letter != NULL)
+        {
+            resources++;
+            letter = strtok(NULL, ",");
+        }
+    }
+    fclose(fp);
+}
+
+int readFileCustomer(char *fileName)
+{
+    FILE *fp = fopen(fileName, "r");
+
+    if (fp == NULL)
+    {
+        printf("Error opening file!\n");
+        return -1;
+    }
+
+    int count = 0;
+    struct stat st;
+    fstat(fileno(fp), &st);
+    char *fileContent = (char *)malloc(((int)st.st_size + 1) * sizeof(char));
+    fileContent[0] = '\0';
+
+    while (!feof(fp))
+    {
+        char line[100];
+        fgets(line, 100, fp);
+        strcat(fileContent, line);
+        count++;
+    }
+
+    fclose(fp);
+    return count;
 }
